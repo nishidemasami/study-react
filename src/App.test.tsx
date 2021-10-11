f046382b3b74dc8d4819d4d3a79dbd8bb298e572
nishidemasami <nishidemasami@gmail.com>
2021-10-11 19:37:33 +0900

    テストを更新

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,26 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import React from "react";
 import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "./redux/store";
 
-test("renders learn react link", () => {
+test("renders learn react link", async () => {
 	render(
 		<Provider store={store}>
 			<App />
 		</Provider>
 	);
-	const element = screen.getByText(/確率/i);
-	expect(element).toBeInTheDocument();
+	const elements = screen.getAllByText(/ようこそ/i);
+	elements.forEach((element) => {
+		expect(element).toBeInTheDocument();
+	});
+
+	userEvent.click(screen.getAllByRole("button")[0]);
+	[/閉じる/i, /ようこそ/i, /カウントアップ/i, /ロゴまわし/i, /ヘルプ/i].forEach(
+		(regex) =>
+			screen
+				.getAllByText(regex)
+				.forEach((element) => expect(element).toBeInTheDocument())
+	);
 });
