80d073e3003a1dddb527ca45c42d38356d03703e
nishidemasami <nishidemasami@gmail.com>
2021-11-22 20:31:09 +0900

    テストを修正

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -2,13 +2,13 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import React from "react";
 import { Provider } from "react-redux";
-import App from "../components/App";
+import WelcomePage from "../components/WelcomePage";
 import { store } from "../redux/store";
 
 test("renders learn react link", async () => {
 	render(
 		<Provider store={store}>
-			<App />
+			<WelcomePage />
 		</Provider>
 	);
 	const elements = screen.getAllByText(/このページはReact練習用ページです/i);
@@ -17,10 +17,9 @@ test("renders learn react link", async () => {
 	});
 
 	userEvent.click(screen.getAllByRole("button")[0]);
-	[/閉じる/i, /ようこそ/i, /カウントアップ/i, /ロゴまわし/i, /ヘルプ/i].forEach(
-		(regex) =>
-			screen
-				.getAllByText(regex)
-				.forEach((element) => expect(element).toBeInTheDocument())
+	[/ようこそ/i, /カウントアップ/i, /ロゴまわし/i, /ヘルプ/i].forEach((regex) =>
+		screen
+			.getAllByText(regex)
+			.forEach((element) => expect(element).toBeInTheDocument())
 	);
 });
